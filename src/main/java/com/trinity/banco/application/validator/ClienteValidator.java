0b3e596058626669eee1bfafaea38d3d0e139ce5
ve312 <felipea201903@gmail.com>
2026-04-10 16:31:05 -0500

    refactor: mejorar validaciones en ClienteValidator

--- src/main/java/com/trinity/banco/application/validator/ClienteValidator.java
+++ src/main/java/com/trinity/banco/application/validator/ClienteValidator.java
@@ -1,4 +1,19 @@
 package com.trinity.banco.application.validator;
 
 public class ClienteValidator {
+    public static void validarEmail(String email) {
+        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$   ";
+        if (email == null || !email.matches(regex)) {
+            throw new RuntimeException("El campo email es obligatorio y debe tener un formato válido");
+        }
+    }
+
+    public static void validarNombre(String nombre, String apellido) {
+        if (nombre == null || nombre.trim().length() < 2) {
+            throw new RuntimeException("El nombre debe tener al menos 2 caracteres");
+        }
+        if (apellido == null || apellido.trim().length() < 2) {
+            throw new RuntimeException("El apellido debe tener al menos 2 caracteres");
+        }
+    }
 }
