0e3d40ba0b45f7e90193808df8c63ea0628a83ef
ve312 <felipea201903@gmail.com>
2026-04-10 16:23:14 -0500

    feat: configurar beans para servicios de dominio

--- src/main/java/com/trinity/banco/infrastructure/config/BeanConfiguration.java
+++ src/main/java/com/trinity/banco/infrastructure/config/BeanConfiguration.java
@@ -1,4 +1,14 @@
 package com.trinity.banco.infrastructure.config;
 
+import com.trinity.banco.domain.Service.NumeroCuentaGenerator;
+import com.trinity.banco.domain.ports.repository.CuentaRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BeanConfiguration {
+    @Bean
+    public NumeroCuentaGenerator numeroCuentaGenerator(CuentaRepository cuentaRepository) {
+        return new NumeroCuentaGenerator(cuentaRepository);
+    }
 }
