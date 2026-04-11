ec21b96a33f2942b9e6c94ea515f25b69b63ba83
ve312 <felipea201903@gmail.com>
2026-04-11 10:34:06 -0500

    feat: implementar capa de persistencia para transacciones (entity, mapper, repository y adapter)

--- src/main/java/com/trinity/banco/infrastructure/entity/TransaccionEntity.java
+++ src/main/java/com/trinity/banco/infrastructure/entity/TransaccionEntity.java
@@ -1,4 +1,37 @@
 package com.trinity.banco.infrastructure.entity;
 
+import com.trinity.banco.domain.model.enums.TipoTransaccion;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transacciones")
+@Getter
+@Setter
 public class TransaccionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String numeroCuenta;
+
+    @Enumerated(EnumType.STRING)
+    private TipoTransaccion tipoTransaccion;
+
+    private BigDecimal monto;
+
+    private BigDecimal saldoAnterior;
+
+    private BigDecimal saldoPosterior;
+
+    private LocalDateTime fecha;
+
+    private String numeroCuentaRelacionada;
+
+    public TransaccionEntity() {}
+
 }
--- src/main/java/com/trinity/banco/infrastructure/repository/TransaccionJpaRepository.java
+++ src/main/java/com/trinity/banco/infrastructure/repository/TransaccionJpaRepository.java
@@ -1,4 +1,11 @@
 package com.trinity.banco.infrastructure.repository;
 
-public interface TransaccionJpaRepository {
+import com.trinity.banco.infrastructure.entity.TransaccionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransaccionJpaRepository extends JpaRepository<TransaccionEntity, Long> {
+
+    List<TransaccionEntity> findByNumeroCuenta(String numeroCuenta);
 }
