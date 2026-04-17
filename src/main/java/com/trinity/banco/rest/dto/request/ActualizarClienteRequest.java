a0d0ae6a6649ee1d2a99b097848e735974d1bf7d
ve312 <felipea201903@gmail.com>
2026-04-16 23:16:36 -0500

    feat: mejorar validaciones en DTOs de entrada

--- src/main/java/com/trinity/banco/rest/dto/request/ActualizarClienteRequest.java
+++ src/main/java/com/trinity/banco/rest/dto/request/ActualizarClienteRequest.java
@@ -2,6 +2,7 @@ package com.trinity.banco.rest.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,10 +12,12 @@ import lombok.Setter;
 public class ActualizarClienteRequest {
     @NotBlank(message = "El nombre es obligatorio")
     @Size(min = 2, message = "El nombre debe tener al menos 2 caracteres")
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$", message = "El nombre solo debe contener letras")
     private String nombres;
 
     @NotBlank(message = "El apellido es obligatorio")
     @Size(min = 2, message = "El apellido debe tener al menos 2 caracteres")
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$", message = "El apellido solo debe contener letras")
     private String apellidos;
 
     @Email(message = "El correo debe tener un formato válido")
--- src/main/java/com/trinity/banco/rest/dto/request/CrearClienteRequest.java
+++ src/main/java/com/trinity/banco/rest/dto/request/CrearClienteRequest.java
@@ -1,9 +1,6 @@
 package com.trinity.banco.rest.dto.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,14 +13,19 @@ public class CrearClienteRequest {
     private String tipoIdentificacion;
 
     @NotBlank(message = "El número de identificación es obligatorio")
+    @Pattern(regexp = "\\d+", message = "El número de identificación solo debe contener números")
+    @Size(min = 6, message = "El minimo de digitos son 6")
+    @Size(max = 10, message = "El maximo de digitos son 10")
     private String numeroIdentificacion;
 
     @NotBlank(message = "El nombre es obligatorio")
     @Size(min = 2, message = "El nombre debe tener al menos 2 caracteres")
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$", message = "El nombre solo debe contener letras")
     private String nombres;
 
     @NotBlank(message = "El apellido es obligatorio")
     @Size(min = 2, message = "El apellido debe tener al menos 2 caracteres")
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$", message = "El apellido solo debe contener letras")
     private String apellidos;
 
     @Email(message = "El correo debe tener un formato válido")
